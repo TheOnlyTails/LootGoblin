4bea3a0cb99ec0887454a5f370ef5203e3693961
TheOnlyTails <shacharzidon@gmail.com>
2021-03-29 10:42:26 +0300

    Added a vararg version of the LootPool$Builder#add function. Bumped patch version.

--- src/main/kotlin/com/theonlytails/loottables/LootPools.kt
+++ src/main/kotlin/com/theonlytails/loottables/LootPools.kt
@@ -1,8 +1,15 @@
 package com.theonlytails.loottables
 
+import net.minecraft.loot.LootEntry
 import net.minecraft.loot.LootPool
 import net.minecraft.loot.conditions.ILootCondition
 import net.minecraft.loot.functions.ILootFunction
+
+@LootTablesDsl
+fun LootEntry.Builder<*>.add(pool: LootPool.Builder): LootPool.Builder = pool.add(this)
+
+@LootTablesDsl
+fun LootPool.Builder.add(vararg entries: LootEntry.Builder<*>) = entries.forEach { add(it) }
 
 @LootTablesDsl
 fun LootPool.Builder.condition(getCondition: () -> ILootCondition.IBuilder): LootPool.Builder = `when`(getCondition())
