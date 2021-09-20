1dea185e64786467966ef8bfbf71ebcf8ed30c47
TheOnlyTails <theonlytails@theonlytails.com>
2021-09-20 23:22:59 +0300

    Tried to fix gradle, currently failing.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 import com.vanniktech.maven.publish.MavenPublishPluginExtension
 import com.vanniktech.maven.publish.SonatypeHost
-import net.minecraftforge.gradle.userdev.UserDevExtension
 import org.gradle.jvm.toolchain.JvmVendorSpec.ADOPTOPENJDK
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.time.Instant.now
@@ -9,14 +8,15 @@ import java.time.format.DateTimeFormatter.ISO_INSTANT
 plugins {
 	idea
 	`java-library`
-	kotlin("jvm") version "1.5.30"
+	kotlin("jvm") version "1.5.31"
 	id("net.minecraftforge.gradle")
 	id("com.vanniktech.maven.publish")
+	id("org.jetbrains.dokka") version "latest.release" // dokka
 }
 
 // Config -> Minecraft
-val forgeVersion: String by extra
-val minecraftVersion: String by extra
+val forgeVersion = findProperty("forge_version") as String
+val minecraftVersion = findProperty("minecraft_version") as String
 val projectVersion = findProperty("VERSION_NAME") as String
 val projectGroup = findProperty("GROUP") as String
 val modId = findProperty("POM_ARTIFACT_ID") as String
@@ -34,12 +34,8 @@ println(
 	""".trimIndent()
 )
 
-dependencies {
-	"minecraft"(group = "net.minecraftforge", name = "forge", version = "$minecraftVersion-$forgeVersion")
-}
-
 // Minecraft
-configure<UserDevExtension> {
+minecraft {
 	mappings("official", minecraftVersion)
 
 	// @Suppress("SpellCheckingInspection")
@@ -57,7 +53,7 @@ configure<UserDevExtension> {
 			// Recommended logging level for the console
 			property("forge.logging.console.level", "debug")
 
-			// Specify the mod id for data generation, where to output the resulting resource, and where to look for existing resources.
+			// Specify the mod ID for data generation, where to output the resulting resource, and where to look for existing resources.
 			args(
 				"--mod",
 				testModId,
@@ -74,8 +70,18 @@ configure<UserDevExtension> {
 				}
 			}
 		}
+
+		all {
+			lazyToken("minecraft_classpath") {
+				library.copyRecursive().resolve().joinToString(File.pathSeparator) { it.absolutePath }
+			}
+		}
 	}
 }
+
+val library: Configuration by configurations.creating
+
+configurations.implementation.get().extendsFrom(library)
 
 repositories {
 	maven {
@@ -83,6 +89,12 @@ repositories {
 		url = uri("https://thedarkcolour.github.io/KotlinForForge/")
 	}
 	mavenCentral()
+	mavenLocal()
+}
+
+dependencies {
+	"minecraft"(group = "net.minecraftforge", name = "forge", version = "$minecraftVersion-$forgeVersion")
+	library(group = "org.jetbrains.kotlin", name = "kotlin-stdlib-jdk8", version = kotlin.coreLibrariesVersion)
 }
 
 // Setup
@@ -114,11 +126,16 @@ tasks.named<Jar>("jar") {
 			"Implementation-Version" to project.version,
 			"Implementation-Vendor" to projectName,
 			"Implementation-Timestamp" to ISO_INSTANT.format(now()),
-			"FMLModType" to "LIBRARY"
+			"FMLModType" to "GAMELIBRARY"
 		)
 	}
 
 	finalizedBy("reobfJar")
+}
+
+// Dokka
+tasks.dokkaHtml.configure {
+	outputDirectory.set(projectDir.resolve("docs"))
 }
 
 // Publishing to maven central
